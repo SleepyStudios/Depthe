41f6c0abd2de81671a1884726dc4601b32349f6a
tudor <7089284+tudddorrr@users.noreply.github.com>
2023-09-30 14:29:48 +0100

    pressure plate -> switch

--- scripts/switch.gd
+++ scripts/switch.gd
@@ -1,12 +1,8 @@
-class_name PressurePlate extends MapObject
+class_name Switch extends MapObject
 
 @onready var sprite = $AnimatedSprite2D
 @export var spikes: Spikes
 
-func _ready():
-	sprite.play("default")
-
 func _on_area_2d_area_entered(_area):
 	sprite.frame = (0 if sprite.frame == 1 else 1)
-	if sprite.frame == 1:
-		spikes.toggle()
+	spikes.toggle()
