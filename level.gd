41927c18ec949927f6f12c2a72a594a8dc177f49
tudor <7089284+tudddorrr@users.noreply.github.com>
2023-09-30 08:43:54 +0100

    fix up/standardise collisions

--- level.gd
+++ level.gd
@@ -9,6 +9,12 @@ func _is_diagonal(original_pos: Vector2, new_pos: Vector2) -> bool:
 	var dir = original_pos.direction_to(new_pos).abs()
 	return dir.x == dir.y
 
+func _get_collision_layer() -> int:
+	for layer_id in range(get_layers_count()):
+		if get_layer_name(layer_id) == 'Collision':
+			return layer_id
+	return -1
+
 func _on_player_drag_ended(original_pos: Vector2, new_pos: Vector2):
 	var local_original_pos = Vector2(local_to_map(original_pos))
 	var local_new_pos = Vector2(local_to_map(new_pos))
@@ -17,6 +23,8 @@ func _on_player_drag_ended(original_pos: Vector2, new_pos: Vector2):
 		var dir = local_original_pos.direction_to(local_new_pos)
 		var next_pos = local_original_pos + dir
 
-		var data = get_cell_tile_data(0, next_pos)
-		if data and not data.get_custom_data("blocked"):
+		var data = get_cell_tile_data(_get_collision_layer(), next_pos)
+		
+		var blocked = data.get_custom_data("blocked") if data else false
+		if not blocked:
 			player.set_new_pos(map_to_local(next_pos))
