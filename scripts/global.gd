4b777fdb5cd9571a40a8bddd617a57637f60be70
tudor <7089284+tudddorrr@users.noreply.github.com>
2023-09-30 18:10:10 +0100

    level switching

--- scripts/global.gd
+++ scripts/global.gd
@@ -4,14 +4,15 @@ var apples_collected = 0
 var current_level = 1
 
 func _get_player():
-	return get_node('../Level/Player')
+	return get_node("../Level%s/Player" % [current_level])
 
 func next_level():
 	apples_collected += _get_player().apples
 	current_level += 1
 	
-	# var new_scene = load("res://scenes/levels/%s.tscn" % [current_level]) 
-	# get_tree().root.add_child(new_scene.instantiate())
+	var new_scene = load("res://scenes/levels/level_%s.tscn" % [current_level]) 
+	get_tree().root.add_child(new_scene.instantiate())
+	get_tree().root.get_node("Level%s" % [current_level - 1]).queue_free()
 
 func kill_player():
 	get_tree().reload_current_scene()
