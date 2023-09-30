87b2666f0e3e9c1992df8411a887a52602531301
tudor <7089284+tudddorrr@users.noreply.github.com>
2023-09-30 23:09:00 +0100

    add some test levels

--- scripts/global.gd
+++ scripts/global.gd
@@ -4,15 +4,20 @@ var apples_collected = 0
 var current_level = 1
 
 func _get_player():
-	return get_node("../Level%s/Player" % [current_level])
+	return get_node("../Level/Player")
 
 func next_level():
 	apples_collected += _get_player().apples
 	current_level += 1
 	
-	var new_scene = load("res://scenes/levels/level_%s.tscn" % [current_level]) 
-	get_tree().root.add_child(new_scene.instantiate())
-	get_tree().root.get_node("Level%s" % [current_level - 1]).queue_free()
+	var new_scene = load("res://scenes/levels/level_%s.tscn" % [current_level])
+	var old_scene = get_tree().root.get_node("Level")
+	old_scene.name = "_"
+
+	var new_scene_instance = new_scene.instantiate()
+	get_tree().get_root().add_child(new_scene_instance)
+	get_tree().set_current_scene(new_scene_instance)
+	old_scene.queue_free()
 
 func kill_player():
 	get_tree().reload_current_scene()
