e35e0515836bb3bc14f6af876d6f87a47459505b
darrelmarek <darrelmarek@hotmail.com>
2023-10-01 14:36:53 -0500

    Adds sounds for switch and crumbling tile

--- scripts/crumbling_tile.gd
+++ scripts/crumbling_tile.gd
@@ -2,6 +2,7 @@ class_name CrumblingTile extends MapObject
 
 @onready var sprite = $AnimatedSprite2D
 @onready var level: Level = $"../Level"
+@onready var crumbling: AudioStreamPlayer = $Crumbling
 
 var crumbled: bool
 
@@ -10,5 +11,6 @@ func _on_area_2d_area_exited(_area):
 		return
 
 	crumbled = true
+	crumbling.play()
 	sprite.play("default")
 	level.add_blocked_tile(position)
--- scripts/switch.gd
+++ scripts/switch.gd
@@ -1,10 +1,12 @@
 class_name Switch extends MapObject
 
 @onready var sprite = $AnimatedSprite2D
+@onready var lever: AudioStreamPlayer = $Lever
 @export var toggleables: Array[MapObject]
 
 func _on_area_2d_area_entered(_area):
 	sprite.frame = (0 if sprite.frame == 1 else 1)
+	lever.play()
 	for toggleable in toggleables:
 		if toggleable.has_method("toggle"):
 			toggleable.toggle()
