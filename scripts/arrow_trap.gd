219db68390d64557c987d604eed33eea4aa834a9
tudor <7089284+tudddorrr@users.noreply.github.com>
2023-10-01 23:39:42 +0100

    arrow trap raycast based

--- scripts/arrow_trap.gd
+++ scripts/arrow_trap.gd
@@ -4,23 +4,15 @@ class_name ArrowTrap extends MapObject
 @onready var area: Area2D = $Area2D
 @onready var arrow = $Arrow
 @onready var player: Player = $"../Player"
+@onready var ray: RayCast2D = $RayCast2D
 
 var will_fire = false
 var original_arrow_pos: Vector2
-var toggle_hold = false
 
 func _ready():
 	player.moved.connect(_on_player_moved)
 	arrow.visible = false
 	original_arrow_pos = arrow.position
-
-func toggle():
-	if toggle_hold:
-		toggle_hold = false
-		return
-
-	will_fire = true
-	toggle_hold = true
 
 func _on_player_moved(_new_pos: Vector2):
 	if will_fire and not arrow.visible:
@@ -28,6 +20,9 @@ func _on_player_moved(_new_pos: Vector2):
 		will_fire = false
 
 func _process(delta):
+	if ray.is_colliding() and not arrow.visible and not will_fire:
+		will_fire = true
+
 	if arrow.visible and not Global.is_game_paused():
 		arrow.position += Vector2.DOWN * 150.0 * delta
 		var map_pos = level.local_to_map(arrow.global_position)
