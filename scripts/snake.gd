51a0abdfad8b9e39780795acc150689e2cf02b77
tudor <7089284+tudddorrr@users.noreply.github.com>
2023-10-01 16:40:13 +0100

    horizontal snake

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -4,18 +4,24 @@ class_name Snake extends MapObject
 @onready var level: Level = $"../Level"
 @onready var sprite: Sprite2D = $Sprite
 
-var dir = 16
+var dir = Vector2(0, 16)
+var horizontal = false
 
 func _ready():
 	player.moved.connect(_on_player_moved)
 
+	horizontal = data.get("horizontal", false)
+	if horizontal:
+		sprite.rotation_degrees = 270
+		dir = Vector2(16, 0)
+
 func _on_player_moved(new_pos: Vector2):
-	if level.is_blocked(level.local_to_map(position + Vector2(0, dir))):
+	if level.is_blocked(level.local_to_map(position + dir)):
 		dir *= -1
 		sprite.flip_v = !sprite.flip_v
 
 	var original_pos = position
-	position.y += dir
+	position += dir
 
 	if level.is_same_tile(player.original_pos, position) and level.is_same_tile(new_pos, original_pos):
 		Global.kill_player()
