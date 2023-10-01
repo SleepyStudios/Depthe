f199e73aa9b366d547aaca1635c7760ec304c630
tudor <7089284+tudddorrr@users.noreply.github.com>
2023-10-01 02:30:57 +0100

    more levels

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -2,6 +2,7 @@ class_name Snake extends MapObject
 
 @onready var player: Player = $"../Player"
 @onready var level: Level = $"../Level"
+@onready var sprite: Sprite2D = $Sprite
 
 var dir = 16
 
@@ -11,6 +12,8 @@ func _ready():
 func _on_player_moved(new_pos: Vector2):
 	if level.is_blocked(level.local_to_map(position + Vector2(0, dir))):
 		dir *= -1
+		sprite.flip_v = !sprite.flip_v
+
 
 	var original_pos = position
 	position.y += dir
