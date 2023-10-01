4b94935f14be8c500331f14d289cea9a5c1508e4
tudor <7089284+tudddorrr@users.noreply.github.com>
2023-10-01 23:55:54 +0100

    tutorial

--- scripts/scene_transition.gd
+++ scripts/scene_transition.gd
@@ -1,12 +1,27 @@
 class_name SceneTransition extends CanvasLayer
 
-@onready var anim: AnimationPlayer = $SceneTransitionAnimationPlayer
+@onready var scene_anim: AnimationPlayer = $SceneTransitionAnimationPlayer
+@onready var tutorial: Label = $Tutorial
+@onready var tutorial_anim: AnimationPlayer = $TutorialAnimationPlayer
+@onready var player: Player = $"../Player"
 
 var transitioning: bool
+var tutorial_dismissed: bool
+
+func _ready():
+	player.moved.connect(_on_player_moved)
+	if Global.current_level == 1:
+		tutorial.text = "Click and drag the player to move"
+		tutorial_anim.play("fade_in")
+
+func _on_player_moved(_new_pos):
+	if Global.current_level == 1 and not tutorial_dismissed:
+		tutorial_anim.play_backwards("fade_in")
+		tutorial_dismissed = true
 
 func begin_transition():
 	transitioning = true
-	anim.play_backwards("scale_down")
+	scene_anim.play_backwards("scale_down")
 
 func _finish_transition():
 	if transitioning:
