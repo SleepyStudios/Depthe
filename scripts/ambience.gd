97141054ace8b30254c0f354460f0a0be85dc708
tudor <7089284+tudddorrr@users.noreply.github.com>
2023-10-02 12:12:29 +0100

    sound mix, ambience

--- scripts/ambience.gd
+++ scripts/ambience.gd
@@ -19,7 +19,7 @@ func _choose_ambience():
   var rand_index = randi_range(0, get_child_count() - 1)
 
   if not get_child(rand_index).playing:
-    get_child(rand_index).volume_db = randf_range(-40, -30)
+    get_child(rand_index).volume_db = randf_range(-40, -15)
     get_child(rand_index).pitch_scale = randf_range(0.9, 1.1)
     get_child(rand_index).play()
 
